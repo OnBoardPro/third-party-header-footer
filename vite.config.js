ef7f77ec609efedf2d3028b6e45e57aa8dc90f28
Anton Frattaroli <frattaro@gmail.com>
2025-01-17 10:16:19 -0500

    fix: send nonce for styles

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react()],
   test: {
     environment: "jsdom"
   },
